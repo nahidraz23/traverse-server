57ea20ef8faa54af190876bddbb4026f02bf664e
nahidraz23 <nzamanraz@gmail.com>
2024-06-03 03:45:11 +0600

    users get api implemented

--- index.js
+++ index.js
@@ -31,6 +31,11 @@ async function run() {
     const usersCollection = client.db('traverseDB').collection('users');
 
     // users related api
+    app.get('/users', async(req, res) => {
+        const result = await usersCollection.find().toArray();
+        res.send(result);
+    })
+
     app.post('/users', async(req, res) => {
         const user = req.body;
         const result = await usersCollection.insertOne(user);
