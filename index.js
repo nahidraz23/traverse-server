98af14e5767185c8158c703cadf4c84d8b298b50
nahidraz23 <nzamanraz@gmail.com>
2024-06-04 18:58:23 +0600

    get tour guides by id api implemented

--- index.js
+++ index.js
@@ -63,6 +63,13 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/guides/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await guidesCollection.findOne(query);
+        res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
