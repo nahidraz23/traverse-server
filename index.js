bee71d7d147b2a81ca0f8a12aa4ac20075eee17e
nahidraz23 <nzamanraz@gmail.com>
2024-06-05 03:59:42 +0600

    wishlist delete api implemented

--- index.js
+++ index.js
@@ -59,6 +59,13 @@ async function run() {
         res.send(result);
     })
 
+    app.delete('/wishlist/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await wishlistCollection.deleteOne(query);
+        res.send(result);
+    })
+
     // packages related api
     app.get('/packageDetails', async(req, res) => {
         const result = await packagesCollection.find().toArray();
